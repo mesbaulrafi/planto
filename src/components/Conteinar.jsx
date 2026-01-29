a3424022c87f6479d6fc4995e2e8883e20dbe9f2
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-01-30 01:36:32 +0600

    fixt: container fixt

--- src/components/Conteinar.jsx
+++ src/components/Conteinar.jsx
@@ -2,7 +2,7 @@ import React, { Children } from 'react'
 
 const Conteinar = ({className,children}) => {
   return (
-    <div className={`max-w-[1120px] m-auto ${className}`}>{children}</div>
+    <div className={`max-w-[1610px] m-auto ${className}`}>{children}</div>
   )
 }
 
