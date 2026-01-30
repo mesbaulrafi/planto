dc6fef2d07a1f3ee87374ae9b84f4b2cde5be161
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-01-30 12:33:43 +0600

    fixt: header Part edit

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -3,6 +3,13 @@ import Images from "../Images";
 import Logo from "/src/assets/Logo.png";
 import Conteinar from "../Conteinar";
 import { Link } from "react-router-dom";
+import { CiSearch } from "react-icons/ci";
+import { IoBagHandleOutline } from "react-icons/io5";
+import { BsListNested } from "react-icons/bs";
+
+
+
+
 
 const Header = () => {
   return (
@@ -14,13 +21,19 @@ const Header = () => {
           </div>
           <div className="pt-6 ">
             <ul className="flex gap-x-15">
-              <Link to={''}><li className="text-2xl font-medium text-white opacity-80">Home</li></Link>
-              <Link to={''}><li className="text-2xl font-medium text-white opacity-80">Plants Type</li></Link>
-              <Link to={''}><li className="text-2xl font-medium text-white opacity-80">More</li></Link>
-              <Link to={''}><li className="text-2xl font-medium text-white opacity-80">Contact</li></Link>
+              <Link to={''}><li className="text-2xl font-medium text-white opacity-80 hover:text-emerald-400">Home</li></Link>
+              <Link to={''}><li className="text-2xl font-medium text-white opacity-80 hover:text-emerald-400">Plants Type</li></Link>
+              <Link to={''}><li className="text-2xl font-medium text-white opacity-80 hover:text-emerald-400">More</li></Link>
+              <Link to={''}><li className="text-2xl font-medium text-white opacity-80 hover:text-emerald-400">Contact</li></Link>
             </ul>
           </div>
-          <div className=""></div>
+          <div className="pt-6 ">
+            <div className="flex gap-x-15">
+              <i className="text-white text-2xl font-bold  hover:text-emerald-300"><CiSearch /></i>
+              <i className="text-white text-2xl font-bold  hover:text-emerald-300"><IoBagHandleOutline /></i>
+              <i className="text-white text-2xl font-bold  hover:text-emerald-300"><BsListNested /></i>
+            </div>
+          </div>
         </div>
       </Conteinar>
     </div>
