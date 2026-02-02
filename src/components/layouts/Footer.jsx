193bd68d28f218af98b418db083bcda513924630
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-02-03 02:30:39 +0600

    fixt: Footer Part edit

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import Conteinar from '../Conteinar'
 import Images from '../Images'
 import FooterLogo from '/src/assets/footerLogo.png'
+import Button from '../Button'
 
 const Footer = () => {
   return (
     <footer className='py-10'>
     <Conteinar className="">
-      <div className="flex ">
+      <div className="flex justify-between">
         {/* 1 */}
         <div className="">
           <Images imgSrc={FooterLogo}/>
-          <p className='pt-11 pb-30 pl-3.5 text-[28px] font-medium text-white w-145'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <p className='pt-11 pb-30 pl-3.5 text-[28px]  text-white w-145'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           <ul className='flex items gap-x-15'>
             <li className='text-white font-extrabold text-2xl'>FB</li>
             <li className='text-white font-extrabold text-2xl'>TW</li>
@@ -20,16 +21,24 @@ const Footer = () => {
         </div>
         {/* 2 */}
         <div className="">
-          <h4 className='text-2xl font-medium text-white'>Quick Link’s</h4>
+          <h4 className='pb-12 text-2xl font-extrabold  text-white'>Quick Link’s</h4>
           <ul>
-            <li className='text-2xl font-medium text-white'>Home</li>
-            <li className='text-2xl font-medium text-white'>Type’s Of plant’s</li>
-            <li className='text-2xl font-medium text-white'>Contact</li>
-            <li className='text-2xl font-medium text-white'>Privacy</li>
+            <li className='text-2xl  text-white pb-6.5'>Home</li>
+            <li className='text-2xl  text-white pb-6.5'>Type’s Of plant’s</li>
+            <li className='text-2xl  text-white pb-6.5'>Contact</li>
+            <li className='text-2xl  text-white'>Privacy</li>
           </ul>
         </div>
         {/* 3 */}
-        <div className=""></div>
+        <div className="">
+            <h4 className='pb-12 text-2xl font-extrabold  text-white'>For Every Update.</h4>
+          <div className="flex items-center justify-between border border-white rounded-lg p-1.5  max-w-[400px]">
+            <input type="text" name="" id="" placeholder='Enter Email' className='bg-transparent text-white outline-none py-px-3 2'  />
+            <Button btnText={'SUBSCRIBE'} className={'bg-white  font-bold text-sm px-6 py-3 rounded-lg '}/>
+          </div>
+          <div className=""><p className='text-2xl font-light text-white pt-50'>planto © all right reserve</p>
+          </div>
+        </div>
       </div>
     </Conteinar>
     </footer>
