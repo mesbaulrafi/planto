be06b69ba8c358607222135b92e4a51e8236dd15
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-02-03 23:19:33 +0600

    edit benner

--- src/components/layouts/Benner.jsx
+++ src/components/layouts/Benner.jsx
@@ -1,32 +1,50 @@
-import React from 'react'
-import Conteinar from '../Conteinar'
-import Button from '../Button'
-import Images from '../Images';
-import PlayBtn from '/src/assets/playBtn.png'
-import { Link } from 'react-router-dom';
-
-
+import React from "react";
+import Conteinar from "../Conteinar";
+import Button from "../Button";
+import Images from "../Images";
+import PlayBtn from "/src/assets/playBtn.png";
+import { Link } from "react-router-dom";
 
 const Benner = () => {
   return (
     <>
-    <div className="bg-[url('/src/assets/bg_home.png')] bg-center bg-no-repeat py-25">
-      <Conteinar>
-        <div className="">
-          {/* Text Top */}
+      <div className="bg-[url('/src/assets/bg_home.png')] bg-center bg-no-repeat py-25">
+        <Conteinar>
           <div className="">
-            <h1 className='text-[118px] text-[#c6c8c5] font-semibold'>Breath Natural </h1>
-            <p className='text-[#c6c8c5] text-2xl font-medium w-[880px] pb-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <div className="flex gap-x-9">
-              <Button btnText={'Explore'} className={'text-[#c6c8c5]'}/>
-              <Link to={''}><Images imgSrc={PlayBtn}/></Link>
+            <div className="w-[900px]">
+              <div className="">
+                {/* Text Top */}
+                <div className="">
+                  <h1 className="text-[118px] text-[#c6c8c5] font-semibold">
+                    Breath Natural{" "}
+                  </h1>
+                  <p className="text-[#c6c8c5] text-2xl font-medium w-[880px] pb-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                  <div className="flex gap-x-9">
+                    <Button btnText={"Explore"} className={"text-[#c6c8c5]"} />
+                    <Link to={""}>
+                      <Images imgSrc={PlayBtn} />
+                    </Link>
+                  </div>
+                </div>
+                <div className="">
+                  <div className="">
+                    
+                  </div>
+                  <p className="">Lorem ipsum dolor sit amet, rerum facere voluptatum impedit fuga saepe? Fugit, doloremque officia!</p>
+                </div>
+              </div>
             </div>
+            {/* ==========1/2======= */}
+            <div className=""></div>
           </div>
-        </div>
-      </Conteinar>
-    </div>
+        </Conteinar>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Benner
+export default Benner;
