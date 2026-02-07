8b2e1368740c467a50b1da25d16b32da083b7aeb
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-02-07 22:54:59 +0600

    edit

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -11,6 +11,7 @@ const Product = ({
 }) => {
   return (
     <div className="">
+      {/* Text Part  */}
       <div className="my-25 border-2 border-white/10 p-8 rounded-[45px] bg-white/5 backdrop-blur-[5px] max-w-[450px] h-[630px] flex flex-col justify-end relative [clip-path:polygon(100%_0,100%_100%,0_100%,0_0,50%_5%)]">
         <div className="">
           <h5 className="opacity-75 text-[38px] my-5">{productTitle}</h5>
@@ -27,6 +28,7 @@ const Product = ({
           </div>
         </div>
       </div>
+      {/* Images Part  */}
       <div className="relative">
         <Images imgSrc={productImg} className={"absolute bottom-[430px] left-0 "} />
       </div>
