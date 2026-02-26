631d16eaec4bd4051a3a600846df1cb37906b7f0
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-02-27 00:40:27 +0600

    edit footer input

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = () => {
         <div className="">
             <h4 className='pb-12 text-2xl font-extrabold  text-white'>For Every Update.</h4>
           <div className="flex items-center justify-between border border-white rounded-lg p-1.5  max-w-[450px]">
-            <input type="text" name="" id="" placeholder='Enter Email' className='bg-transparent text-white outline-none '  />
+            <input type="text" name="" id="" placeholder='Enter Email' className='bg-transparent text-white outline-none p-2'  />
             <Button btnText={'SUBSCRIBE'} className={'bg-white text-black! font-bold text-sm rounded-lg '}/>
           </div>
           <div className=""><p className='text-2xl font-light text-white pt-50'>planto © all right reserve</p>
