f85062f22bf9322392103f7012ec4a2d44af12e8
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-04-01 11:31:52 +0600

    add : Contact part

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact Page</div>
+   <>
+   <div className="py-20">
+    <div className="">
+      <h2 className='font-inter font-semibold text-[55px] text-center'>Contact Us </h2>
+    </div>
+   </div>
+   </>
   )
 }
 
