596398f840d42568676677c04dac277e1a7c1e55
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-04-03 19:45:02 +0600

    remove

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -5,17 +5,7 @@ import Button from '../Button'
 const Contact = () => {
   return (
    <>
-   <div className="py-20">
-    <div className="">
-      <h2 className='font-inter font-semibold text-[55px] text-center pb-25'>Contact Us </h2>
-      <Blurdiv className={'max-w-[714px] m-auto '}>
-        <input type="text" name="" id="" placeholder='Name'/>
-        <input type="email" name="" id="" placeholder='Name'/>
-        <textarea name="" id="" placeholder='Message'></textarea>
-        <Button btnText={'Submit'}/>
-      </Blurdiv>
-    </div>
-   </div>
+  
    </>
   )
 }
