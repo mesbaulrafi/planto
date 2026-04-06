d214c607cf3649cdae998b59a6f598f129e05614
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-04-07 01:03:56 +0600

    edit

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -5,7 +5,7 @@ import Button from '../Button'
 
 const Contact = () => {
   return (
-    <section className="py-20 bg-[#0d110d] min-h-screen flex flex-col items-center">
+    <section className="py-20 bg-[#0d110d]  flex flex-col items-center">
       <div className="container mx-auto px-4">
         <h2 className='font-inter font-semibold text-[48px] text-center pb-16 text-white'>
           Contact Us
