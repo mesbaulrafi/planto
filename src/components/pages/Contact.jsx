c581fb15802ee2cbc7feb1e7037cd70c172b24ae
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-04-10 02:06:36 +0600

    add

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -11,7 +11,33 @@ const Contact = () => {
           Contact Us
         </h2>
         
-        
+        <Blurdiv className="max-w-[500px] m-auto flex flex-col gap-5">
+          {/* Name Input */}
+          <input 
+            type="text" 
+            placeholder='Name' 
+            className="w-full bg-[#5e635b] border-none outline-none p-4 rounded-[20px] text-white placeholder:[#a0a29e]"
+          />
+          
+          {/* Email Input */}
+          <input 
+            type="email" 
+            placeholder='Email' 
+            className="w-full bg-[#5e635b] border-none outline-none p-4 rounded-[20px] text-white placeholder:[#a0a29e]"
+          />
+          
+          {/* Message Textarea */}
+          <textarea 
+            placeholder='Message' 
+            rows="6"
+            className="w-full bg-[#3d4339] border-none outline-none p-4 rounded-[25px] text-white placeholder:[#a0a29e] resize-none"
+          ></textarea>
+          
+          {/* Submit Button Centering */}
+          <div className="flex justify-center mt-2">
+             <button className='bg-[#373d32] text-white text-4xl font-bold rounded-[40px]  backdrop-blur-[5px] py-4 px-[70px]'>Submit</button>
+          </div>
+        </Blurdiv>
       </div>
     </section>
   )
