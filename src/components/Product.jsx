d26b6256c863b34d2d3aeb60fc5081f2c66f6493
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-05-12 00:54:11 +0600

    plant type responsiv

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -10,27 +10,30 @@ const Product = ({
   productDesription,
 }) => {
   return (
-    <div className="">
+    <div className="flex flex-col items-center w-full sm:w-[48%] lg:w-[30%]">
       {/* Text Part  */}
-      <div className="my-25 border-2 border-white/10 p-8 rounded-[45px] bg-white/5 backdrop-blur-[5px] max-w-[450px] h-[630px] flex flex-col justify-end relative [clip-path:polygon(100%_0,100%_100%,0_100%,0_0,50%_5%)]">
+      <div className="mt-20 sm:mt-25 border-2 border-white/10 p-5 sm:p-8 rounded-[35px] sm:rounded-[45px] bg-white/5 backdrop-blur-[5px] w-full max-w-[450px] h-auto sm:h-[630px] flex flex-col justify-end relative [clip-path:polygon(100%_0,100%_100%,0_100%,0_0,50%_5%)]">
         <div className="">
-          <h5 className="opacity-75 text-[38px] my-5">{productTitle}</h5>
-          <p className="opacity-75 text-2xl py-9">{productDesription}</p>
+          <h5 className="opacity-75 text-[24px] sm:text-[30px] lg:text-[38px] my-3 sm:my-5">{productTitle}</h5>
+          <p className="opacity-75 text-lg sm:text-xl lg:text-2xl py-5 sm:py-9">{productDesription}</p>
           <div className="flex justify-between opacity-75 items-center">
             <div className="">
-              <p className="opacity-75 text-[38px] ">{productPrice}</p>
+              <p className="opacity-75 text-[24px] sm:text-[30px] lg:text-[38px]">{productPrice}</p>
             </div>
-            <div className="border-2 p-3.5 rounded-xl cursor-pointer">
+            <div className="border-2 p-3 sm:p-3.5 rounded-xl cursor-pointer">
               <Link to={""}>
-                <IoBagHandleSharp className="text-2xl opacity-75" />
+                <IoBagHandleSharp className="text-xl sm:text-2xl opacity-75" />
               </Link>
             </div>
           </div>
         </div>
       </div>
       {/* Images Part  */}
-      <div className="relative">
-        <Images imgSrc={productImg} className={"absolute bottom-[430px] left-0 "} />
+      <div className="relative w-full">
+        <Images
+          imgSrc={productImg}
+          className={"absolute bottom-[220px] sm:bottom-[430px] left-1/2 -translate-x-1/2 sm:left-0 sm:translate-x-0 w-[200px] sm:w-auto"}
+        />
       </div>
     </div>
   );
--- src/components/pages/PlantsType.jsx
+++ src/components/pages/PlantsType.jsx
@@ -16,108 +16,91 @@ const PlantsType = () => {
     <>
       <Container className={"py-10"}>
         <div className="">
-          {/* Our Top Selling  Hedding */}
-          <div className="py-17.5 text-center  flex justify-center">
-            <div className="relative inline-block ">
-              <div className="absolute top-0 right-0 w-16 h-16 border-t-4 border-r-4 border-[#abffb369] rounded-tr-[20px]"></div>
-              <div className="absolute bottom-0 left-0 w-16 h-16 border-b-4 border-l-4 border-[#abffb3aa] rounded-bl-[20px]"></div>
-              <h2 className="text-[55px] font-semibold px-5 ">
+          {/* Our Top Selling Heading */}
+          <div className="py-10 sm:py-17.5 text-center flex justify-center">
+            <div className="relative inline-block">
+              <div className="absolute top-0 right-0 w-10 h-10 sm:w-16 sm:h-16 border-t-4 border-r-4 border-[#abffb369] rounded-tr-[20px]"></div>
+              <div className="absolute bottom-0 left-0 w-10 h-10 sm:w-16 sm:h-16 border-b-4 border-l-4 border-[#abffb3aa] rounded-bl-[20px]"></div>
+              <h2 className="text-[32px] sm:text-[42px] lg:text-[55px] font-semibold px-5">
                 Our Top Selling
               </h2>
             </div>
           </div>
-          {/* Our Top Selling  Hedding End */}
+          {/* Our Top Selling Heading End */}
+
           <div className="">
             {/* Product Details */}
-            <div className="border-2 border-white/10  p-8 rounded-[45px] bg-white/5 backdrop-blur-[5px]">
-              <div className="flex items-center justify-between">
-                <div className="">
-                  <Images imgSrc={PlTree1} />
+            <div className="border-2 border-white/10 p-5 sm:p-8 rounded-[30px] sm:rounded-[45px] bg-white/5 backdrop-blur-[5px]">
+              <div className="flex flex-col lg:flex-row items-center justify-between gap-8">
+                {/* Image */}
+                <div className="w-full lg:w-auto flex justify-center">
+                  <Images imgSrc={PlTree1} className={"max-w-[300px] sm:max-w-[400px] lg:max-w-none w-full"} />
                 </div>
-                <div className="w-[610px] ">
-                  <h3 className="text-4xl opacity-75">
-                    We Have Small And Best O2 Plants Collection’s
+                {/* Text */}
+                <div className="w-full lg:w-[610px]">
+                  <h3 className="text-2xl sm:text-3xl lg:text-4xl opacity-75 text-center lg:text-left">
+                    We Have Small And Best O2 Plants Collection's
                   </h3>
-                  <p className="py-10.5 text-2xl opacity-75">
+                  <p className="py-6 sm:py-10.5 text-lg sm:text-xl lg:text-2xl opacity-75 text-center lg:text-left">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                     do eiusmod tempor incididunt ut labore et dolore magna
                     aliqua
                   </p>
-                  <p className="text-2xl opacity-75">
+                  <p className="text-lg sm:text-xl lg:text-2xl opacity-75 text-center lg:text-left">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                     do eiusmod tempor incididunt ut labore et dolore magna
                     aliqua. Ut enim ad minim veniam.
                   </p>
-                  <Button btnText={"Explore"} className={"mt-10.5"} />
+                  <div className="flex justify-center lg:justify-start">
+                    <Button btnText={"Explore"} className={"mt-8 sm:mt-10.5"} />
+                  </div>
                 </div>
               </div>
             </div>
             {/* Product Details End */}
 
-            {/* Top Selling Product Part  */}
-            <div className="py-40">
-              {/* Fast Row  */}
-              <div className="flex items-center justify-between">
-                {/* p1 */}
+            {/* Top Selling Product Part */}
+            <div className="py-20 sm:py-40">
+              <div className="flex flex-col sm:flex-row flex-wrap items-center justify-center sm:justify-between gap-y-32 sm:gap-y-16 gap-x-4">
                 <Product
                   productImg={PT1}
                   productTitle={"Calathea plant"}
-                  productDesription={
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-                  }
+                  productDesription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit"}
                   productPrice={"Rs. 359/-"}
                 />
-                {/* p2 */}
                 <Product
                   productImg={PT2}
                   productTitle={"Calathea plant"}
-                  productDesription={
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-                  }
+                  productDesription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit"}
                   productPrice={"Rs. 359/-"}
                 />
-                {/* p3 */}
                 <Product
                   productImg={PT3}
                   productTitle={"Calathea plant"}
-                  productDesription={
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-                  }
+                  productDesription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit"}
                   productPrice={"Rs. 359/-"}
                 />
-              </div>
-              {/* Fast row End  */}
-              <div className="flex items-center justify-between">
-                {/* p1 */}
                 <Product
-                  productImg={PT4} 
+                  productImg={PT4}
                   productTitle={"Calathea plant"}
-                  productDesription={
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-                  }
+                  productDesription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit"}
                   productPrice={"Rs. 359/-"}
                 />
-                {/* p2 */}
                 <Product
                   productImg={PT5}
                   productTitle={"Calathea plant"}
-                  productDesription={
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-                  }
+                  productDesription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit"}
                   productPrice={"Rs. 359/-"}
                 />
-                {/* p3 */}
                 <Product
                   productImg={PT6}
                   productTitle={"Calathea plant"}
-                  productDesription={
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-                  }
+                  productDesription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit"}
                   productPrice={"Rs. 359/-"}
                 />
               </div>
             </div>
-            {/* Top Selling Product Part  */}
+            {/* Top Selling Product Part End */}
           </div>
         </div>
       </Container>
