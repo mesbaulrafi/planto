c4fafa223fcc2068045198777293bd56e10b65ab
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-07-28 21:42:40 +0600

    Home page fixt

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Benner from '../layouts/Benner'
-
+import PlantsType from './PlantsType'
 
 const Home = () => {
   return (
    <>
    <Benner/>
+   <PlantsType/>
    
    </>
   )
