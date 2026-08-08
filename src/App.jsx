21ae4457268e783d30f4c634632697c86cad1db8
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-08-09 01:34:37 +0600

    add

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import RootLayouts from "./components/layouts/RootLayouts";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Error from "./components/pages/Error";
+import PlantsType from "./components/pages/PlantsType";
+import Contact from "./components/pages/Contact";
+import More from "./components/pages/More";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<RootLayouts />}>
+          <Route index element={<Home />} />
+          <Route path="/plantsType" element={<PlantsType />} />
+          <Route path="/more" element={<More />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
