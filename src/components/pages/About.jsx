e045205947de448ddafc1c9eb4fb6fe07478651e
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-08-12 00:39:33 +0600

    about

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='bg-gray-100'>About</div>
+   <>
+   <section>
+    <div className="container">
+     <h1>About Us</h1>
+     <p>Welcome to our company! We are a team of passionate individuals dedicated to providing the best services possible.</p>
+    </div>
+   </section>
+   </>
   )
 }
 
